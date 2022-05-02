27dcf8e6c527879438e9ccdce3cb99c24e7f32e3
addmix <addmixtaylor@gmail.com>
2022-05-02 02:20:46 -0600

    Fixed upright restorative force

--- PhysicsPlayer.gd
+++ PhysicsPlayer.gd
@@ -27,14 +27,15 @@ func _integrate_forces(state : PhysicsDirectBodyState3D) -> void:
 	if Raycast.is_colliding():
 		#prevents bobbing up and down
 		legs_force(delta, state)
-#		movement_damping(delta, state)
+		movement_damping(delta, state)
+		ground_movement(transformed, delta)
 		keep_upright(delta, state)
-#		rotation_damping(delta, state )
-#		ground_movement(transformed, delta)
-#		keep_upright(delta, state)
+		rotation_damping(delta, state)
+		
 	else:
 		#when not on ground, don't damp, it messes with other physics
 		air_movement(transformed, delta)
+	
 	
 	if (state.total_gravity.length() > 0):
 		pass
@@ -47,20 +48,17 @@ func legs_force(delta : float, body_state : PhysicsDirectBodyState3D) -> void:
 	var distance = (global_transform.origin + global_transform.basis.y * -0.3).distance_to(Raycast.get_collision_point())
 	#allows crouching
 	var leg_height : float = (0.7 * float(!Input.is_action_pressed("crouch"))) + (0.3 * float(Input.is_action_pressed("crouch")))
-#	print(leg_height)
 	#basically makes jumping the same as stiff-legging
 	var min_force : float = jump_force * float(Input.is_action_pressed("jump"))
-#	print(min_force)
 	var force : float = clamp(range_lerp(distance, 0, leg_height, max_leg_force, min_force), 0, max_leg_force)
-#	print(force)
-	apply_impulse(global_transform.basis.y * -1.0, global_transform.basis.y * force * body_state.total_gravity.length())
+	apply_force(global_transform.basis.y * -1.0, global_transform.basis.y * force * body_state.total_gravity.length())
 
 func movement_damping(delta : float, body_state : PhysicsDirectBodyState3D) -> void:
 	#transform to local y axis
 	var local_y_velocity : float = (global_transform.basis.inverse() * linear_velocity).y
 	var damp_force := -local_y_velocity * damp
 	
-	apply_impulse(global_transform.basis.y * -1, global_transform.basis.y * damp_force * body_state.total_gravity.length() * delta)
+	apply_force(global_transform.basis.y * -1, global_transform.basis.y * damp_force * body_state.total_gravity.length())
 
 func keep_upright(delta : float, body_state : PhysicsDirectBodyState3D) -> void:
 	var up : Vector3 = -body_state.total_gravity.normalized()
@@ -68,14 +66,15 @@ func keep_upright(delta : float, body_state : PhysicsDirectBodyState3D) -> void:
 	#prevents 0 gravity from causing divide by 0 errors
 	up += global_transform.basis.y * float(up == Vector3.ZERO)
 	
-	var left_axis := up.cross(last_direction)
-	var desired_orientation := Basis(left_axis, up, last_direction).orthonormalized()
+	var right_axis := up.cross(last_direction)
+	#why did 4.0 make it necessary for these to be negative?
+	var desired_orientation := Basis(right_axis, -up, -last_direction).orthonormalized()
 	
-	var force : Quaternion = MathUtils.quat_to_axis_angle((desired_orientation * global_transform.basis.inverse()).get_rotation_quaternion())
-	apply_torque_impulse(Vector3(force.x, force.y, force.z) * force.w * rotation_force * delta)
+	var force : Quaternion = MathUtils.quat_to_axis_angle((desired_orientation * (global_transform.basis.inverse())).get_rotation_quaternion())
+	apply_torque(Vector3(force.x, force.y, force.z) * force.w * rotation_force)
 
 func rotation_damping(delta : float, _body_state : PhysicsDirectBodyState3D) -> void:
-	apply_torque_impulse(-angular_velocity * rotation_damp * delta)
+	apply_torque(-angular_velocity * rotation_damp)
 
 #direction is already in global space
 func ground_movement(direction : Vector3, delta : float) -> void:
@@ -83,7 +82,7 @@ func ground_movement(direction : Vector3, delta : float) -> void:
 	var force := (desired_velocity - linear_velocity) * max_leg_force
 	
 	#full force when standing still or slowing down
-	apply_central_impulse(force * delta)
+	apply_central_force(force)
 
 func air_movement(_direction : Vector3, _delta : float) -> void:
 	pass
